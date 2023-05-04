122db6721c0bbde0ccd1345a65d362a03b03f0cd
Pineapple217 <rosierjules@gmail.com>
2023-05-04 20:36:02 +0200

    get track route

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,23 @@
-use axum::{routing::get, Router, Server};
+use axum::{
+    extract::{Path, State},
+    routing::get,
+    Router, Server,
+};
+use rspotify::{model::TrackId, prelude::*, ClientCredsSpotify, Credentials};
 
 #[tokio::main]
-
 async fn main() {
-    let router = Router::new().route("/", get(root_get));
+    let creds = Credentials::from_env().unwrap();
+    let spotify = ClientCredsSpotify::new(creds);
+    println!("Getting spotify token...");
+    spotify.request_token().await.unwrap();
+    println!("Token optained.");
+
+    let router = Router::new()
+        .route("/", get(root_get))
+        .route("/api/track/:id", get(get_track))
+        .with_state(spotify);
+
     let server = Server::bind(&"0.0.0.0:8080".parse().unwrap()).serve(router.into_make_service());
     let addr = server.local_addr();
     println!("Listening on {addr}");
@@ -13,4 +27,10 @@ async fn main() {
 
 async fn root_get() -> &'static str {
     "hi :3"
+}
+
+async fn get_track(Path(id): Path<String>, State(spotify): State<ClientCredsSpotify>) -> String {
+    let track_uri = TrackId::from_id(&id).unwrap();
+    let track = spotify.track(track_uri).await.unwrap();
+    format!("{} by {}", track.name, track.artists[0].name)
 }
