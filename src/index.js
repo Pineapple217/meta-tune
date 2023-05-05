40ee70c7b70c030627a89a39454fa43e3da8c323
Pineapple217 <rosierjules@gmail.com>
2023-05-05 20:41:04 +0200

    js cleanup

--- src/index.js
+++ src/index.js
@@ -12,23 +12,12 @@ https://github.com/Pineapple217/Muze-Site
 import { createApp } from "https://unpkg.com/petite-vue@0.2.2/dist/petite-vue.es.js";
 
 createApp({
-  // exposed to all expressions
-  count: 0,
   track_id: "",
-  name: "",
-  // getters
-  get plusOne() {
-    return this.count + 1;
-  },
+  track: null,
 
   async onEnter() {
-    this.count++;
-    console.log(this.track_id);
-    let x = await fetch(`./api/track/${this.track_id}`);
-    let y = await x.json();
-    this.name = y.name;
+    let respone = await fetch(`./api/track/${this.track_id}`);
+    let track_json = await respone.json();
+    this.track = track_json;
   },
 }).mount();
-
-// const main = () => {};
-// addEventListener("DOMContentLoaded", main);
