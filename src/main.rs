081567efef46ecb8d092ae2a7c0e85c9bcc18354
Pineapple217 <rosierjules@gmail.com>
2023-05-05 14:34:18 +0200

    static files and state

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,21 @@
 use axum::{
     extract::{Path, State},
+    http::Response,
+    response::{Html, IntoResponse},
     routing::get,
-    Router, Server,
+    Json, Router, Server,
 };
-use rspotify::{model::TrackId, prelude::*, ClientCredsSpotify, Credentials};
+// use log::info;
+use rspotify::{
+    model::{FullTrack, TrackId},
+    prelude::*,
+    ClientCredsSpotify, Credentials,
+};
+
+#[derive(Clone)]
+struct AppState {
+    spotify: ClientCredsSpotify,
+}
 
 #[tokio::main]
 async fn main() {
@@ -12,32 +24,61 @@ async fn main() {
     println!("Getting spotify token...");
     spotify.request_token().await.unwrap();
     println!("Token optained.");
+    let app_state = AppState { spotify: spotify };
 
     let router = Router::new()
         .route("/", get(root_get))
+        .route("/index.js", get(indexmjs_get))
+        .route("/index.css", get(indexcss_get))
         .route("/api/track/:id", get(get_track))
-        .with_state(spotify);
+        .with_state(app_state.clone());
 
     let server = Server::bind(&"0.0.0.0:8080".parse().unwrap()).serve(router.into_make_service());
     let addr = server.local_addr();
     println!("Listening on {addr}");
+    // info!("Listening on {addr}");
 
     server.await.unwrap();
 }
 
-async fn root_get() -> &'static str {
-    "hi :3"
+// async fn root_get() -> &'static str {
+//     "hi :3"
+// }
+async fn root_get() -> impl IntoResponse {
+    let markup = tokio::fs::read_to_string("src/index.html").await.unwrap();
+
+    Html(markup)
 }
 
-async fn get_track(Path(id): Path<String>, State(spotify): State<ClientCredsSpotify>) -> String {
+async fn indexmjs_get() -> impl IntoResponse {
+    let markup = tokio::fs::read_to_string("src/index.js").await.unwrap();
+
+    Response::builder()
+        .header("content-type", "application/javascript;charset=utf-8")
+        .body(markup)
+        .unwrap()
+}
+
+async fn indexcss_get() -> impl IntoResponse {
+    let markup = tokio::fs::read_to_string("src/index.css").await.unwrap();
+
+    Response::builder()
+        .header("content-type", "text/css;charset=utf-8")
+        .body(markup)
+        .unwrap()
+}
+
+async fn get_track(Path(id): Path<String>, State(app_state): State<AppState>) -> Json<FullTrack> {
+    let spotify = app_state.spotify;
     let track_uri = TrackId::from_id(&id).unwrap();
     let track = spotify.track(track_uri).await.unwrap();
     let artist = spotify
         .artist(track.artists[0].id.as_ref().unwrap().as_ref())
         .await
         .unwrap();
-    format!(
-        "{} by {}\ngenre: {:?}",
-        track.name, track.artists[0].name, artist.genres
-    )
+    // format!(
+    //     "{} by {}\ngenre: {:?}",
+    //     track.name, track.artists[0].name, artist.genres
+    // )
+    Json(track)
 }
