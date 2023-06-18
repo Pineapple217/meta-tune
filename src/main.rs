6be0248c87adb19fe92c5253dabad94e7e97b417
Pineapple217 <rosierjules@gmail.com>
2023-06-18 16:54:10 +0200

    better logs

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,18 @@
 mod handlers;
 mod models;
+use axum::body::Body;
+use axum::http::Request;
+use tower_http::trace::TraceLayer;
+use tower_http::{
+    trace::{DefaultMakeSpan, DefaultOnResponse},
+    LatencyUnit,
+};
+use tracing::{info, metadata::LevelFilter};
+use tracing::{Level, Span};
+use tracing_subscriber::EnvFilter;
 
 use axum::{routing::get, Router, Server};
 use dotenv::dotenv;
-use env_logger::Env;
-use log::info;
-// use log::{debug, error, info, trace, warn};
 use rspotify::{
     // prelude::*,
     ClientCredsSpotify,
@@ -19,8 +26,14 @@ use models::AppState;
 async fn main() {
     dotenv().ok();
 
-    let env = Env::default().filter_or("LOG_LEVEL", "warn");
-    env_logger::init_from_env(env);
+    let filter = EnvFilter::builder()
+        .with_default_directive(LevelFilter::WARN.into())
+        .with_env_var("LOG_LEVEL")
+        .from_env_lossy();
+    tracing_subscriber::fmt()
+        .with_env_filter(filter)
+        .with_target(false)
+        .init();
 
     let creds = Credentials::from_env().expect("Spotify credentials");
     let conf = Config {
@@ -37,6 +50,28 @@ async fn main() {
         .route("/index.js", get(handlers::indexmjs_get))
         .route("/index.css", get(handlers::indexcss_get))
         .route("/api/track/:id", get(handlers::get_track))
+        .layer(
+            TraceLayer::new_for_http()
+                .make_span_with(
+                    DefaultMakeSpan::new()
+                        .include_headers(true)
+                        .level(Level::DEBUG),
+                )
+                // .on_request(DefaultOnRequest::new().level(Level::INFO))
+                .on_request(|request: &Request<Body>, _span: &Span| {
+                    tracing::info!(
+                        "request: {} {} {:?}",
+                        request.method(),
+                        request.uri().path(),
+                        request.headers()
+                    )
+                })
+                .on_response(
+                    DefaultOnResponse::new()
+                        .level(Level::INFO)
+                        .latency_unit(LatencyUnit::Millis),
+                ),
+        )
         .with_state(app_state.clone());
 
     let server = Server::bind(&"0.0.0.0:3000".parse().unwrap()).serve(router.into_make_service());
